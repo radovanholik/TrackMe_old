240d2d0f6510547f730f17f51948f11c0ff8eee5
Radovan Holík <radovanholik@gmail.com>
2018-12-12 17:15:36 +0100

    Created Remote modul

---
+++ Remote/src/main/java/cz/trackme/remote/UserRemoteImpl.kt
@@ -0,0 +1,21 @@
+package cz.trackme.remote
+
+import cz.radovanholik.data.model.UserEntity
+import cz.radovanholik.data.repository.UserRemote
+import io.reactivex.Completable
+import io.reactivex.Observable
+
+class UserRemoteImpl : UserRemote {
+
+    override fun saveUser(user: UserEntity): Completable {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getUser(userId: String): Observable<UserEntity> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getUsers(userIds: List<String>): Observable<List<UserEntity>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
